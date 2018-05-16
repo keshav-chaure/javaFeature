665d71540de20b2c0c25f622d2c33658a5787418
Keshav C Chaure <keshav.chaure@Yash.com>
2018-05-16 13:00:15 +0530

    added java 8 interface

--- src/main/java/com/yash/kc/javaFeatures/App.java
+++ src/main/java/com/yash/kc/javaFeatures/App.java
@@ -1,5 +1,9 @@
 package com.yash.kc.javaFeatures;
 
+import com.yash.kc.java8.LedLight;
+import com.yash.kc.java8.LightEmiter;
+import com.yash.kc.java8.SyskaLight;
+
 /**
  * Hello world!
  *
@@ -10,8 +14,21 @@ public class App
     {
         System.out.println( "Hello World!" );
         com.yash.kc.java5.Example app=new com.yash.kc.java5.Example();
-        app.showGeneric();
-        app.enumTest();
+       // app.showGeneric();
+        //app.enumTest();
         //app.showGeneric();
+      
+     // java 8 interface default method    
+        SyskaLight led=new SyskaLight();
+      //   led.color="blue"; color is defined in interface so default public static final
+      //  led.color="blue";
+         System.out.println("led color : "+ led.color);
+        System.out.println("led color : "+ led.getColor()); // behiviour from abstract method
+        System.out.println("led color : "+ LightEmiter.color); // constant form interface 
+      
+      
+      //java 8 interface static method      
+    //  LightEmiter.consumes();
+      
     }
 }
